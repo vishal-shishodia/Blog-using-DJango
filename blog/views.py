1b32cc3c3d42e60844bd67e0aa9a861ff0bfe034
Vishal Shishodia <vishalshishodia97@gmail.com>
2020-08-22 14:44:52 +0530

    first commit

---
+++ blog/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render
+from blog.models import BlogPost
+from django.shortcuts import get_object_or_404
+from django.db.models import Q
+
+def create_blog_view(request):
+	context={}
+	user=request.user
+	if not user.is_authenticated:
+		return redirect('must_authenticate')
+	form=CreateBlogPostForm(request.POST or None, request.Files or None)
+	if form.is_valid():
+		obj=form.save(commit=False)
+		author=Account.objects.filter(email=user.email).first()
+		obj.author=author
+		obj.save()
+	context['form']=form
+	return render(request,'blog/create_blog.html',context)
+
+def detail_blog_view(request,slug):
+	context={}
+	blog_post=get_object_or_404(BlogPost,slug=slug)
+	context['blog_post']=blog_post
+	return render(request,'blog/detail_blog.html',context)
+
+
+def edit_blog_view(request,slug):
+	context={}
+	user=request.user
+	if not user.is_authenticated:
+		return redirect('must_authenticated')
+	blog_post=get_object_or_404(BlogPost,slug=slug)
+	if request.POST:
+		form=UpdateBlogPostForm(request.POST or None,request.FILES or None,instance=blog_post)
+		if form.is_valid():
+			obj=form.save(commit=False)
+			obj.save()
+			context['successmessage']='updated'
+	form=UpdateBlogPostForm(
+			initial={
+			'title':blog_post.title,
+			'body':blog_post.body,
+			'image':blog_post.image
+			}
+		)
+	context['form']=form
+	return render(request,'blog/edit_blog.html',context)
+
+def get_blog_queryset(query=None):
+	queryset=[]
+	queries=query.split(' ')
+	for q in queries:
+		posts=BlogPost.objects.filter(
+			Q(title__icontains=q)|
+			Q(body__icontains=q)
+			).distinct()
+		for post in posts:
+			queryset.append(post)
+
+	return list(set(queryset))
